970fdd15428c5a4a4d11cb542608910a13c03fce
hongwehl <hongwehl@lucent.com>
2017-11-20 11:02:00 +0800

    1) rm chr, lnccr.sh, lncmd.sh, lsdb.sh, lsdv.sh; 2) update README, ccr, genBP, setuptools.sh, smartbp.pl; 3) add aiaudit to audit ai rtdb

--- lsdb.sh
+++
@@ -1,1 +0,0 @@
-psql -Uscncraft -At -c "select * from rtdb_app where db_name not in ('NDB','BDB','HLRV','HLRNV')"
--- setuptools.sh
+++ setuptools.sh
@@ -40,7 +40,7 @@ tempbindir="$tempbasedir/bin"
 configfile="$bindir/CONFIGURE"
 
 # copy tools
-toollist="LogCMB teel teela eteela dama damaf edamaf rstama trbp rstspa rstdb ldb pldb getdb.py ldfrm audit ccri ccru ccrt ngini nginu ngint ccre createdb stopall.sh keygen ckcip chr openrc closerc clrc genfrm refrc ckcus genBP updsql ckop mutemeas tl logcmb stbl"
+toollist="LogCMB teel teela eteela dama damaf edamaf rstama trbp rstspa rstdb ldb pldb getdb.py ldfrm audit aiaudit ccri ccru ccrt ngini nginu ngint ccre createdb stopall.sh keygen ckcip openrc closerc clrc genfrm refrc ckcus genBP genAuditBP updsql ckop mutemeas tl logcmb stbl"
 
 if [ ! -d $basedir ]
 then
